436fde1080c747cff557c0a1f10d7ae5f242b69b
prezes229 <luszkiewiczmarek51@gmail.com>
2026-09-13 14:58:37 +0200

    zadanie 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    // Zadanie 1
+    System.out.println("Ania");
+    System.out.println("Bartek");
+    System.out.println("Kasia");
+
+}
