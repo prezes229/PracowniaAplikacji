ae49ad0b92790df87be720d66457dbd58e2f32a5
prezes229 <luszkiewiczmarek51@gmail.com>
2026-09-17 18:32:14 +0200

    Zadanie 1

--- ProjektTestowy/zadania02/main.js
+++ ProjektTestowy/zadania02/main.js
@@ -5,30 +5,18 @@ public class Main {
 
     Scanner scanner = new Scanner(System.in);
 
-    // Zadanie 1
-    System.out.println("Zadanie 1");
-    System.out.println("Podaj liczbę:");
-    int liczba = scanner.nextInt();
 
-    if (liczba % 3 == 0) {
-    System.out.println("Liczba jest podzielna przez 3");
+
+// Zadanie 3
+System.out.println("\nZadanie 3");
+System.out.println("Podaj pierwszą liczbę:");
+double liczba1 = scanner.nextDouble();
+
+System.out.println("Podaj drugą liczbę:");
+double liczba2 = scanner.nextDouble();
+
+if (liczba1 > liczba2) {
+    System.out.println("Największa liczba: " + liczba1);
 } else {
-    System.out.println("Liczba nie jest podzielna przez 3");
-}
-
-// Zadanie 2
-System.out.println("\nZadanie 2");
-System.out.println("Podaj pierwszy bok:");
-double bok1 = scanner.nextDouble();
-
-System.out.println("Podaj drugi bok:");
-double bok2 = scanner.nextDouble();
-
-System.out.println("Podaj trzeci bok:");
-double bok3 = scanner.nextDouble();
-
-if (bok1 + bok2 > bok3 && bok1 + bok3 > bok2 && bok2 + bok3 > bok1) {
-    System.out.println("Można zbudować trójkąt");
-} else {
-    System.out.println("Nie można zbudować trójkąta");
+    System.out.println("Największa liczba: " + liczba2);
 }
