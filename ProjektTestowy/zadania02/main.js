b575fa3ee5597f02592a144fd858d7624aed3d31
prezes229 <luszkiewiczmarek51@gmail.com>
2026-09-17 18:35:31 +0200

    Zadanie 6

--- ProjektTestowy/zadania02/main.js
+++ ProjektTestowy/zadania02/main.js
@@ -5,49 +5,16 @@ public class Main {
 
     Scanner scanner = new Scanner(System.in);
 
-    // Zadanie 5
-    System.out.println("\nZadanie 5");
-    System.out.println("Podaj numer miesiąca:");
-    int miesiac = scanner.nextInt();
+    // Zadanie 6
+    System.out.println("\nZadanie 6");
+    System.out.println("Podaj swoje imię:");
+    String podaneImie = scanner.next();
 
-    switch (miesiac) {
-    case 1:
-        System.out.println("Styczeń");
-        break;
-    case 2:
-        System.out.println("Luty");
-        break;
-    case 3:
-        System.out.println("Marzec");
-        break;
-    case 4:
-        System.out.println("Kwiecień");
-        break;
-    case 5:
-        System.out.println("Maj");
-        break;
-    case 6:
-        System.out.println("Czerwiec");
-        break;
-    case 7:
-        System.out.println("Lipiec");
-        break;
-    case 8:
-        System.out.println("Sierpień");
-        break;
-    case 9:
-        System.out.println("Wrzesień");
-        break;
-    case 10:
-        System.out.println("Październik");
-        break;
-    case 11:
-        System.out.println("Listopad");
-        break;
-    case 12:
-        System.out.println("Grudzień");
-        break;
-    default:
-        System.out.println("Nieprawidlowy numer miesiaca");
-    }
+    String mojeImie = "Maciek";
+
+    if (podaneImie.equals(mojeImie)) {
+    System.out.println("Twoje imię jest takie samo jak moje");
+} else {
+    System.out.println("Twoje imię jest inne niż moje");
+}
 
