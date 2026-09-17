b12a9976061e8cd75f5f5e8de6e1e688a2234a6a
prezes229 <luszkiewiczmarek51@gmail.com>
2026-09-17 18:30:26 +0200

    Zadanie 2

--- ProjektTestowy/zadania02/main.js
+++ ProjektTestowy/zadania02/main.js
@@ -15,3 +15,20 @@ public class Main {
 } else {
     System.out.println("Liczba nie jest podzielna przez 3");
 }
+
+// Zadanie 2
+System.out.println("\nZadanie 2");
+System.out.println("Podaj pierwszy bok:");
+double bok1 = scanner.nextDouble();
+
+System.out.println("Podaj drugi bok:");
+double bok2 = scanner.nextDouble();
+
+System.out.println("Podaj trzeci bok:");
+double bok3 = scanner.nextDouble();
+
+if (bok1 + bok2 > bok3 && bok1 + bok3 > bok2 && bok2 + bok3 > bok1) {
+    System.out.println("Można zbudować trójkąt");
+} else {
+    System.out.println("Nie można zbudować trójkąta");
+}
